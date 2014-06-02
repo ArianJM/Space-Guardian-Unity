d0f2b5512b2221ebdcbc34d3cb7b81d8d2b2e436
Arián Jiménez Majidí <arianjm@gmail.com>
2014-06-02 23:37:02 +0200

    Added soundtrack, deleted ProjectSettings.asset
    Added a soundtrack downloaded from Jamendo.
    Deleted ProjectSettings\ProjectSettings.asset for containing data about
    my signing certificate.

--- Assets/Scripts/DestroyByContact.js
+++ Assets/Scripts/DestroyByContact.js
@@ -2,11 +2,32 @@
 
 var explosion : GameObject;
 var playerExplosion : GameObject;
+var scoreValue : int;
+
+private var gameController : GameController;
+
+function Start() {
+	var gameControllerObject : GameObject = GameObject.FindWithTag("GameController");
+	if (gameControllerObject != null)
+		gameController = gameControllerObject.GetComponent(GameController);
+	if (gameController == null)
+		Debug.Log ("Cannot find 'GameController' script");
+}
 
 function OnTriggerEnter(other : Collider){
 	if(other.tag == "Boundary") return;
 	Instantiate(explosion, transform.position, transform.rotation);
-	if (other.tag == "Player") Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+	if (other.tag == "Player"){
+		Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+		gameController.GameOver();
+	}
+	else gameController.AddScore(scoreValue);
 	Destroy(other.gameObject);
 	Destroy(gameObject);
+}
+
+function OnTriggerExit(other : Collider){
+	if(other.tag == "Boundary" && !gameController.IsGameOver()){
+		gameController.AddScore(scoreValue/2);
+	}
 }
--- Assets/Scripts/GameController.js
+++ Assets/Scripts/GameController.js
@@ -6,8 +6,26 @@ var spawnWait : float;
 var startWait : float;
 var waveWait : float;
 
+var scoreText : GUIText;
+var restartText : GUIText;
+var gameOverText : GUIText;
+
+private var gameOver : boolean;
+private var restart : boolean;
+private var score : int;
+
 function Start () {
+	gameOver = false;
+	restart = false;
+	restartText.text = "";
+	gameOverText.text = "";
+	score = 0;
+	UpdateScore();
     SpawnWaves ();
+}
+
+function Update(){
+	if (restart && Input.touchCount >= 1) Application.LoadLevel(Application.loadedLevel);
 }
 
 function SpawnWaves () {
@@ -15,12 +33,34 @@ function SpawnWaves () {
 	while (true){
 		for ( var i : int = 0 ; i < hazardCount ; i++){
 			var randX = Random.Range(0.1f, 0.9f);
-			var spawnPosition : Vector3 = Camera.main.ViewportToWorldPoint(new Vector3(randX, 1.3f, 1.0f));
+			var spawnPosition : Vector3 = Camera.main.ViewportToWorldPoint(new Vector3(randX, 1.1f, 1.0f));
 			spawnPosition.y = 0.0f;
 		    var spawnRotation : Quaternion = Quaternion.identity;
 		    Instantiate (hazard, spawnPosition, spawnRotation);
 		    yield WaitForSeconds (spawnWait);
 		}
 		yield WaitForSeconds (waveWait);
+		if (gameOver){
+			restartText.text = "Tap to restart";
+			restart = true;
+			break;
+		}
 	}
+}
+
+function AddScore (addScore : int){
+	score += addScore;
+	UpdateScore();
+}
+
+function UpdateScore () {
+	scoreText.text = "Score: " + score;
+}
+
+function IsGameOver (){
+	return gameOver;
+}
+function GameOver (){
+	gameOverText.text = "Game Over!";
+	gameOver = true;
 }
